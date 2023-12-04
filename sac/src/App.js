956a42e2a3ed065ccdd32e87a127c590814166ba
Eunku Kang <kegoo1997@gmail.com>
2023-12-05 08:51:23 +0900

    tailwind + font install

--- sac/src/App.js
+++ sac/src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <body>
-        <h1> Hello World </h1>
-        <p>This is a <strong style={{ fontWeight: 'bold' }}>boldText</strong></p>
-        <p> This is a paragraph</p>
-      </body>
-    </div>
-  );
+    return (
+        <div className="h-96 w-96 flex justify-center">
+            <div className="h-44 w-44 bg-black"></div>
+
+        </div>
+
+    );
 }
 
 export default App;
