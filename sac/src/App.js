4034c8acd65cbb70fda499fc486c54ba5b71aa6c
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-12-04 16:40:36 +0900

    test conflic 해결

--- sac/src/App.js
+++ sac/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <strong>Learn React</strong>
+          Learn React
         </a>
       </header>
       <body>
