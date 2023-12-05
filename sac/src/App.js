9b8c1b3488d58b4750776aedf993481d662e410a
Eunku Kang <kegoo1997@gmail.com>
2023-12-05 10:15:02 +0900

    clean up no user file

--- sac/src/App.js
+++ sac/src/App.js
@@ -1,9 +1,6 @@
-import Header from "./components/Header";
-
 function App() {
     return (
         <div className="h-max">
-            <Header/>
             <p>안녕하세요</p>
             <p className="font-bold">두꺼운 폰트</p>
             <p className="text-lime-900">색깔있는 폰트</p>
