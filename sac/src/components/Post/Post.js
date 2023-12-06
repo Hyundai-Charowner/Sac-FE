d55719ba737b6d26e2b2e093a29bc3c4ffa0621e
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-12-06 22:54:09 +0900

    네이밍, 간격 수정

--- sac/src/components/Post/Post.js
+++ sac/src/components/Post/Post.js
@@ -13,9 +13,9 @@ const Post = ({ imageUrl, username, timestamp, topic, title, content, commentCou
                 <div className='post-info'>
                     <div className='post-user-image'>
                         {imageUrl ? (
-                            <img src={imageUrl} alt='User Image' />
+                            <img src={imageUrl} alt='User' />
                         ) : (
-                            <img src={userDefaultImage} alt='User Default Image' />
+                            <img src={userDefaultImage} alt='User Default' />
                         )}
                     </div>
                     <div className='post-info-body'>
@@ -35,18 +35,18 @@ const Post = ({ imageUrl, username, timestamp, topic, title, content, commentCou
             <div className='post-footer'>
                 <div className='post-footer-buttons'>
                     <button className='post-comment-button'>
-                        <img src={commentImage} alt='User Image' />
+                        <img src={commentImage} alt='Comment' className='footer-icon'/>
                         <div className='footer-text'>{commentCount}</div>
                     </button>
                     <button className='post-comment-button'>
-                        <img src={heartTrueImage} alt='User Image' />
+                        <img src={heartTrueImage} alt='Heart' className='footer-icon'/>
                         <div className='footer-text'>{heartCount}</div>
                     </button>
                     <button className='post-comment-button'>
-                        <img src={shareImage} alt='User Image' />
+                        <img src={shareImage} alt='Share' className='footer-icon'/>
                     </button>
                     <button className='post-comment-button'>
-                        <img src={viewsImage} alt='User Image' />
+                        <img src={viewsImage} alt='Views' className='footer-icon'/>
                         <div className='footer-text'>{viewsCount}</div>
                     </button>
                 </div>
