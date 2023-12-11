6b157befa2277c005a19b3248e67253b16c31f10
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-12-11 17:49:41 +0900

    Mypage기초셋팅, header 디자인 일부 수정

---
+++ sac/src/Page/MyPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/Home.css';
+import Header from "../components/Header/Header";
+
+function MyPage() {
+
+  return (
+    <div className="App">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="body">
+      </div>
+    </div>
+  );
+}
+
+export default MyPage;
+
