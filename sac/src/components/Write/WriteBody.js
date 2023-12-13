6396770ef23d40c18beb87b3858cae7edd6079cf
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-12-13 19:27:29 +0900

    등록하기 버튼 동작 생성

--- sac/src/components/Write/WriteBody.js
+++ sac/src/components/Write/WriteBody.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
 import axiosInstance from "../../utils/api.js";
+import Modal from "react-modal";
+import customModalStyles from '../../styles/Modal';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '../../styles/Write.css';
-
 import { Editor } from '@toast-ui/react-editor';
 import { useNavigate } from "react-router-dom";
 
@@ -10,6 +11,9 @@ const WriteBody = () => {
     const editorRef = useRef(null);
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
+    const [showModal, setShowModal] = useState(false); // 모달 표시 여부
+    const [showFooterText, setShowFooterText] = useState(false); // footer-text 표시 여부
+    const [shakeEditor, setShakeEditor] = useState(false); // editor 흔들림 여부
     const navigate = useNavigate();
 
     const handleContentChange = () => {
@@ -22,6 +26,18 @@ const WriteBody = () => {
     };
 
     const handleSubmit = async () => {
+        if (!title.trim() || !body.trim()) {
+            setShowFooterText(true);
+            setShakeEditor(true);
+
+            // 0.2초 후 흔들림 효과 제거
+            setTimeout(() => {
+                setShakeEditor(false);
+            }, 200);
+
+            return;
+        }
+
         try {
             const response = await axiosInstance.post("/posts", {
                 board_id: 1,
@@ -30,21 +46,26 @@ const WriteBody = () => {
             });
 
             console.log('API 응답:', response.data);
-            navigate('/');
+
+            // 성공 시 모달 표시
+            setShowModal(true);
         } catch (error) {
             console.error('API 요청 실패:', error);
             console.error('token', localStorage.getItem('jwtToken'));
         }
     };
-    
-    
 
     const handleCancel = () => {
         navigate('/');
     };
 
+    const handleCloseModal = () => {
+        setShowModal(false);
+        navigate('/');
+    };
+
     return (
-        <div className="markdown-editor">
+        <div className={`markdown-editor ${shakeEditor ? 'shake' : ''}`}>
             <input
                 className="title-input"
                 type="text"
@@ -67,9 +88,25 @@ const WriteBody = () => {
                 />
             </div>
             <div className="footer">
-                <button className="submit-button-post" onClick={handleSubmit}>등록하기</button>
-                <button className="submit-button-cancel" onClick={handleCancel}>취소하기</button>
+                {showFooterText && <div className="footer-text">내용을 입력해주세요.</div>}
+                <div className="footer-button">
+                    <button className="submit-button-post" onClick={handleSubmit}>등록하기</button>
+                    <button className="submit-button-cancel" onClick={handleCancel}>취소하기</button>
+                </div>
             </div>
+
+            <Modal
+                isOpen={showModal}
+                onRequestClose={handleCloseModal}
+                style={customModalStyles}
+                backgroundColor="white"
+            >   
+                <div className="post-modal">
+                    <div className="post-modal-text">게시글이 등록되었습니다.</div>
+                    <button onClick={handleCloseModal} className="post-modal-button">확인</button>
+                </div>
+            </Modal>
+
         </div>
     );
 }
