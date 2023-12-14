c77d77fa7b68fc05ca14aa4acf807978405b8694
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-12-14 10:10:53 +0900

    Markdown 형식으로 post content 변경

--- sac/src/components/Post/PostDetail.js
+++ sac/src/components/Post/PostDetail.js
@@ -5,13 +5,16 @@ import '../../styles/Post.css';
 import PostReply from './PostReply';
 import ReplyEdit from './ReplyEdit';
 
-const PostDetail = ({ imageUrl, username, timestamp, topic, title, content, commentCount, heartCount, viewsCount }) => {
+const PostDetail = ({ imageUrl, username, timestamp, topic, title, content}) => {
+    
+    content = "<h3>이승민 실화냐?? 그는 진짜 전설이다..</h3>"
+    
     return (
         <div className='post-detail'>
             <Header imageUrl={imageUrl} username={username} timestamp={timestamp} topic={topic} />
 
             <Body title={title} content={content} />
-            <div className='divider' />
+            <div className='divider-detail' />
             <PostReply />
             <ReplyEdit />
         </div>
--- sac/src/components/Post/PostFooter.js
+++ sac/src/components/Post/PostFooter.js
@@ -4,6 +4,7 @@ import heartTrueImage from '../../assets/image/heartTrue.png';
 import heartFalseImage from '../../assets/image/heartFalse.png';
 import shareImage from '../../assets/image/share.png';
 import viewsImage from '../../assets/image/views.png';
+import '../../styles/Post.css';
 
 const Footer = ({ commentCount, heartCount, viewsCount }) => {
     const [isHeartClicked, setIsHeartClicked] = useState(false);
@@ -18,18 +19,18 @@ const Footer = ({ commentCount, heartCount, viewsCount }) => {
             <div className='post-footer-buttons'>
                 <div className='post-button'>
                     <img src={commentImage} alt='Comment' className='footer-icon'/>
-                    <div className='footer-text'>{commentCount}</div>
+                    <div className='post-footer-text'>{commentCount}</div>
                 </div>
                 <button className='post-button' onClick={handleHeartClick}>
                     <img src={isHeartClicked ? heartTrueImage : heartFalseImage} alt='Heart' className='footer-icon'/>
-                    <div className='footer-text'>{heartCount}</div>
+                    <div className='post-footer-text'>{heartCount}</div>
                 </button>
                 <button className='post-button'>
                     <img src={shareImage} alt='Share' className='footer-icon'/>
                 </button>
                 <div className='post-button'>
                     <img src={viewsImage} alt='Views' className='footer-icon'/>
-                    <div className='footer-text'>{viewsCount}</div>
+                    <div className='post-footer-text'>{viewsCount}</div>
                 </div>
             </div>
         </div>
