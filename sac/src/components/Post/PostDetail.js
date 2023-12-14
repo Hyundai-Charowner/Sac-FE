ad3e8ef1928006e9b38fd9b7ea28341f7abd436d
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-12-15 02:04:27 +0900

    댓글 리프레시 성공

--- sac/src/components/Post/PostDetail.js
+++ sac/src/components/Post/PostDetail.js
@@ -1,50 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import axiosInstance from '../../utils/api.js';
 import Header from './PostHeader';
 import Body from './PostBody';
-import '../../styles/Post.css';
 import Reply from '../commons/Reply';
 import ReplyEdit from './ReplyEdit';
-import axiosInstance from "../../utils/api.js";
-import { useEffect, useState } from 'react';
 
 const PostDetail = ({ postId }) => {
+  const [post, setPost] = useState(null);
+  const [replies, setReplies] = useState(null);
 
-    const [post, setPost] = useState(null);
-    const [reply, setReply] = useState(null);
-
-    useEffect(() => {
-        const fetchPost = async () => {
-            try {
-                const response = await axiosInstance.get(`/posts/${postId}`);
-                setPost(response.data.post);
-                setReply(response.data.reply);
-            } catch (error) {
-                console.error('데이터 가져오기 실패:', error);
-            }
-        };
-
-        fetchPost();
-
-    }, [postId]);
-
-    if (!post) {
-        return <div>Loading...</div>;
+  const fetchPostAndReplies = async () => {
+    try {
+      const response = await axiosInstance.get(`/posts/${postId}`);
+      setPost(response.data.post);
+      setReplies(response.data.reply);
+    } catch (error) {
+      console.error('데이터 가져오기 실패:', error);
     }
+  };
 
-    return (
-        <div className='post-detail'>
-            <Header imageUrl={post.user_image} username={post.user_name} timestamp={post.created_date} topic={post.board_category} />
-            <Body title={post.post_head} content={post.post_content} />
-            <div className='divider-detail' />
-            {reply && reply.map((replyObject, index) => (
-                <div className="post-reply" key={index}>
-                    <Reply reply={replyObject} />
-                </div>
-            ))}
-            <ReplyEdit postId={post.post_id}/>
-        </div>
-    );
+  const updateReplies = async () => {
+    try {
+        await fetchPostAndReplies();
+        // 화면을 맨 위로 스크롤
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    } catch (error) {
+      console.error('Failed to fetch replies:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPostAndReplies();
+  }, [postId]);
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="post-detail">
+      <Header
+        imageUrl={post.user_image}
+        username={post.user_name}
+        timestamp={post.created_date}
+        topic={post.board_category}
+      />
+      <Body title={post.post_head} content={post.post_content} />
+      <div className="divider-detail" />
+      {replies &&
+        replies.map((replyObject, index) => (
+          <div className="post-reply" key={index}>
+            <Reply reply={replyObject} />
+          </div>
+        ))}
+      <ReplyEdit postId={post.post_id} updateReplies={updateReplies} />
+    </div>
+  );
 };
-
-
 
 export default PostDetail;
